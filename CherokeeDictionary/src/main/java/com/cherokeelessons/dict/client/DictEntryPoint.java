cb1c777fc3b62849032dc7c557f15650f6b5ba6e
mjoyner <mjoyner@vbservices.net>
2015-08-02 15:02:19 -0400

    initial project entrypoint and module

---
+++ CherokeeDictionary/src/main/java/com/cherokeelessons/dict/client/DictEntryPoint.java
@@ -0,0 +1,12 @@
+package com.cherokeelessons.dict.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class DictEntryPoint implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+
+	}
+
+}
